4ec78fbe213f9ba1268a3513994a0c54704db8cc
Elakov Nikita <89816960858@mail.ru>
2020-11-04 14:32:31 +0300

    add report Allure

--- src/test/java/test/DeliveryCard.java
+++ src/test/java/test/DeliveryCard.java
@@ -2,9 +2,13 @@ package test;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
 import data.DataGenerator;
+import io.qameta.allure.selenide.AllureSelenide;
 import lombok.val;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import page.LoginPage;
@@ -15,9 +19,18 @@ import static data.DataGenerator.Registration.generateByCard;
 public class DeliveryCard {
 
     @BeforeEach
-    void setUpAll() {
-        Configuration.browser = "firefox";
+    public void openPage() {
+        Configuration.browser = "chrome";
         Configuration.startMaximized = Boolean.parseBoolean("true");
+    }
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
     }
 
     @Test
